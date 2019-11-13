efb463da0468237223cc437005ea3623aa9d6cbc
kurone-kito <krone@kit.black>
2019-11-14 00:14:33 +0900

    🥳

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Admin, DataProvider } from "react-admin";
+import { Admin, DataProvider, ListGuesser, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import "./App.css";
 
@@ -7,6 +7,10 @@ const dataProvider: DataProvider = jsonServerProvider(
   "http://jsonplaceholder.typicode.com"
 );
 
-const App: React.FC = () => <Admin dataProvider={dataProvider} />;
+const App: React.FC = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={ListGuesser} />
+  </Admin>
+);
 
 export default App;
